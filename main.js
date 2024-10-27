6fab1818fa109bad9b477aa6ea4d39ac96f50fe0
Joaoluisribeir <144291980+Joaoluisribeir@users.noreply.github.com>
2024-10-27 15:21:21 -0300

    Jldev

---
+++ main.js
@@ -0,0 +1,19 @@
+const hamburge = document.querySelector('#hamburger');
+const navlist = document.querySelector('#nlist');
+
+hamburge.addEventListener('click', () => {
+  nlist.classList.toggle('show')
+  hamburger.classList.toggle('active');
+}
+)
+
+function md() {
+  const body = document.querySelector("#body");
+  body.classList.toggle("back");
+}
+function wpp( ) {
+  window.location.href="#Abount";
+}
+function c() {
+  window.location.href="https://Wa.me/558698857-1559"
+}
